ea33413dc1a8291d98b1ce14d7c0147a3a6b35c3
Simon A Bull <simonbull1@gmail.com>
2018-07-24 16:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   
   var obj = {prop: 1}
+  obj.object = 
   var newObj = Object.assign({}, obj)
   
   return object + newObj
