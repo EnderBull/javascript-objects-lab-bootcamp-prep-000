43fe89801fdd22ebf84cab02654ca5bbcc0696a2
Simon A Bull <simonbull1@gmail.com>
2018-07-24 15:39:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function Objects(object){
+  recipes = {}
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+  object = {key: value}
+  var newObj = Object.assign({}, object)
+  
+  return object + newObj
+}
